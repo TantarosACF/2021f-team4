5aa57c850fdd260e153d42ce3c27fe06eeaa2576
megalbra <megalbra@mtu.edu>
2021-10-09 12:09:01 -0400

    Added animation to the avatar
    Added different animations based on what direction the avatar is moving
    Updated script to reflect this change in the animation

--- Fantasy Forge/Assets/Scripts/CharacterMovement.cs
+++ Fantasy Forge/Assets/Scripts/CharacterMovement.cs
@@ -9,6 +9,8 @@ public class CharacterMovement : MonoBehaviour
 
     public Rigidbody2D rb;
 
+    public Animator animator;
+
     Vector2 movement;
 
     // Update is called once per frame
@@ -17,6 +19,10 @@ public class CharacterMovement : MonoBehaviour
         //Input
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     private void FixedUpdate()
